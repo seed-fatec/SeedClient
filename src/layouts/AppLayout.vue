d814017981954f843f3b00bc22533ea32d98f70e
gabrielbotandev <gabrielficciobotan@gmail.com>
2025-04-16 22:33:58 -0300

    feat: update app layout with new structure

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import Header from '~/blocks/Header.vue'
+</script>
 
 <template>
-  <div class="min-h-screen bg-neutral-200 flex items-center justify-center">
-    <RouterView />
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <main class="flex-grow bg-neutral-200 flex items-center justify-center">
+      <RouterView />
+    </main>
   </div>
 </template>
